2742b580daf7c9a2a867cf72e3ae411933e1f797
Demreik <121563554+Demreik@users.noreply.github.com>
2023-02-16 16:50:24 +0200

    version 1

--- Calculator_v1/Program.cs
+++ Calculator_v1/Program.cs
@@ -11,6 +11,60 @@ namespace Calculator_v1
         static void Main(string[] args)
         {
             Console.Write("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию:");
+            Console.WriteLine("1. Сложение");
+            Console.WriteLine("2. Вычетание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+            Console.WriteLine("5. Квадратный корень");
+
+            Console.Write("Введите номер операции (1-5): ");
+            int choise = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+
+            double result = 0.0;
+
+            switch(choise)
+            {
+                case 1:
+                    result = number1 + number2;
+                    break;
+                case 2:
+                    result = number1 - number2;
+                    break;
+                case 3:
+                    result = number1 * number2;
+                    break;
+                case 4:
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("Ошибка, нельзя делить на ноль");
+                        return;
+                    }
+                    result = number1 / number2;
+                    break;
+                case 5:
+                    if (number1 < 0)
+                    {
+                        Console.WriteLine("Ошибка, чсло не может быть отрецательным");
+                        return;
+                    }
+                    result = Math.Sqrt(number1);
+                    break;
+                default:
+                    Console.WriteLine("Ошибка, нет такой операции");
+                    return;
+
+            }
+
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine("Нажмите кнопку чтобы выйти");
+            Console.ReadKey();
         }
     }
 }
