dc19e61bf1af499c3102c83a392b44ff0d93eb2d
Demreik <121563554+Demreik@users.noreply.github.com>
2023-02-17 17:41:46 +0200

    Ввел валидацию и цикл на повтор использования

--- Calculator_v1/Program.cs
+++ Calculator_v1/Program.cs
@@ -10,68 +10,202 @@ namespace Calculator_v1
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите первое число: ");
-            double number1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Выберите операцию:");
-            Console.WriteLine("1. Сложение");
-            Console.WriteLine("2. Вычетание");
-            Console.WriteLine("3. Умножение");
-            Console.WriteLine("4. Деление");
-            Console.WriteLine("5. Квадратный корень");
-
-            Console.Write("Введите номер операции (1-5): ");
-            int choise = Convert.ToInt32(Console.ReadLine());
-
-          
-
-
-            double result = 0.0;
-
-            switch(choise)
+            while (true)
             {
-                case 1:
-                    Console.Write("Введите второе число: ");
-                    double number2 = Convert.ToDouble(Console.ReadLine());
-                    result = number1 + number2;
-                    break;
-                case 2:
-                    Console.Write("Введите второе число: ");
-                    double number3 = Convert.ToDouble(Console.ReadLine());
-                    result = number1 - number3;
-                    break;
-                case 3:
-                    Console.Write("Введите второе число: ");
-                    double number4 = Convert.ToDouble(Console.ReadLine());
-                    result = number1 * number4;
-                    break;
-                case 4:
-                    Console.Write("Введите второе число: ");
-                    double number5 = Convert.ToDouble(Console.ReadLine());
-                    if (number5 == 0)
-                    {
-                        Console.WriteLine("Ошибка, нельзя делить на ноль");
-                        return;
-                    }
-                    result = number1 / number5;
-                    break;
-                case 5:
-                    if (number1 < 0)
-                    {
-                        Console.WriteLine("Ошибка, чсло не может быть отрецательным");
-                        return;
-                    }
-                    result = Math.Sqrt(number1);
-                    break;
-                default:
-                    Console.WriteLine("Ошибка, нет такой операции");
-                    return;
+                double number1 = 0.0;
 
+                while (true)
+                {
+                    Console.Write("Введите первое число: ");
+                    if (double.TryParse(Console.ReadLine(), out number1))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Ошибка, необходимо ввести число");
+                }
+
+                Console.WriteLine("Выберите операцию:");
+                Console.WriteLine("1. Сложение");
+                Console.WriteLine("2. Вычетание");
+                Console.WriteLine("3. Умножение");
+                Console.WriteLine("4. Деление");
+                Console.WriteLine("5. Квадратный корень");
+
+                int choise = 0;
+
+                while (true)
+                {
+                    Console.Write("Введите номер операции (1-5): ");
+                    if (int.TryParse(Console.ReadLine(), out choise))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Ошибка, необходимо ввести число");
+                }
+
+                double result = 0.0;
+                double number2 = 0.0;
+
+
+                switch (choise)
+                {
+                    case 1:
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                result = number1 + number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+                        }
+                        break;
+                    case 2:
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                result = number1 - number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+                        }
+                        break;
+                    case 3:
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                result = number1 * number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+                        }
+                        break;
+                    case 4:
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                if (number2 == 0)
+                                {
+                                    Console.WriteLine("Ошибка, нельзя делить на ноль");
+                                    return;
+                                }
+                                result = number1 / number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+
+                        }
+                        break;
+                    case 5:
+                        if (number1 < 0)
+                        {
+                            Console.WriteLine("Ошибка, чсло не может быть отрецательным");
+                            return;
+                        }
+                        result = Math.Sqrt(number1);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка, нет такой операции");
+                        return;
+
+                }
+
+                /*
+                while(true)
+                {
+                    if(choise == 1)
+                    {
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                result = number1 + number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+                        }
+
+                    }
+
+                    if (choise == 2)
+                    {
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                result = number1 - number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+                        }
+
+                    }
+
+                    if (choise == 3)
+                    {
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                result = number1 * number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+                        }
+
+                    }
+
+                    if (choise == 4)
+                    {
+                        while (true)
+                        {
+                            Console.Write("Введите второе число: ");
+                            if (double.TryParse(Console.ReadLine(), out number2))
+                            {
+                                result = number1 / number2;
+                                break;
+                            }
+                            Console.WriteLine("Ошибка, необходимо ввести число");
+                        }
+
+                    }
+
+                    if (choise == 5)
+                    {
+                        if (number1 < 0)
+                        {
+                            Console.WriteLine("Ошибка, число не может быть отрицательным");
+                            break;
+                        }
+                        result = Math.Sqrt(number1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка, нет такой операции");
+                    }
+                    Console.WriteLine($"Result: {result}");
+                }
+                */
+
+                Console.WriteLine($"Результат: {result}");
+                Console.Write("Нажмите R что бы воспользоваться калькулятором еще раз" +
+                    ", или нажмите другую клавишу для выхода: ");
+                string input = Console.ReadLine();
+                if (input.ToUpper() != "R")
+                {
+                    break;
+                }
             }
-
-            Console.WriteLine($"Result: {result}");
-            Console.WriteLine("Нажмите кнопку чтобы выйти");
-            Console.ReadKey();
         }
     }
 }
